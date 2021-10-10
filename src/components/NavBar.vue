0a28908282b852c98d683f9ab28bcad232fe15c7
Nick <dreadnaughtlin@gmail.com>
2021-10-10 11:13:53 +0800

    add NavBar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,94 @@
+<template>
+  <header>
+        <nav class="navbar py-0">
+            <div class="container-fluid px-0">
+                <a href="#" class="navbar-brand ms-4">
+                    <img src="../assets/images/logo.svg" alt="logo">
+                </a>
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link login" href="#">LOGIN</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link signup" href="#">SIGN UP</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link wish" href="#">WISH LIST</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link cart" href="#">CART</a>
+                    </li>
+                    <li class="nav-item">
+                        <span class="nav-link search" href="#"></span>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <nav class="navbar menu">
+            <div class="container justify-content-center">
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">PRODUCTS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">SHOPPING GUIDE</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+  nav li a.nav-link {
+    color: #000;
+    line-height: 60px;
+    height: 60px;
+    font-size: 13px;
+    padding: 0 1.625rem;
+    .nav-link.login {
+    background: url(../assets/images/lock.svg) no-repeat 0 50%;
+    }
+    .nav-link.signup {
+    background: url(../assets/images/account.svg) no-repeat 0 50%;
+    }
+    .nav-link.wish {
+    background: url(../assets/images/heart.svg) no-repeat 0 50%;
+    }
+    .nav-link.cart {
+    background: url(../assets/images/cart.svg) no-repeat 0 50%;
+    }   
+}
+
+nav ul li span.nav-link.search {
+    width: 60px;
+    height: 60px;
+    background: url(../assets/images/search.svg) no-repeat 50% 50%;
+    background-color: #000;
+    background-size: 24px 24px;
+    cursor: pointer;
+}
+
+nav.navbar.menu {
+    border-top: 1px solid #ddd;
+    border-bottom: 1px solid #ddd;
+    li a.nav-link {
+    padding: 0 2.5rem;
+    font-size: 12px;
+    }
+}
+
+</style>
---
+++ src/views/Products.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavBar></NavBar>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "Products",
+  components: {
+    NavBar,
+  },
+};
+</script>
