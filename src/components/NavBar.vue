a1892815b80a536d777c3bf6fffa001f7b1cad20
Nick <dreadnaughtlin@gmail.com>
2021-10-18 21:51:54 +0800

    modify style

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,51 @@
+<template>
+  <header>
+        <nav class="navbar py-0">
+            <div class="container-fluid px-0">
+                <a href="#" class="navbar-brand ms-4">
+                    <img src="../assets/images/logo.svg" alt="logo">
+                </a>
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link login" href="#">LOGIN</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link signup" href="#">SIGN UP</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link wish" href="#">WISH LIST</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link cart" href="#">CART</a>
+                    </li>
+                    <li class="nav-item">
+                        <span class="nav-link search" href="#"></span>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <nav class="navbar menu">
+            <div class="container justify-content-center">
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link">HOME</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">PRODUCTS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">SHOPPING GUIDE</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
+
 <style scoped lang="scss">
 
   nav li a.nav-link {
@@ -5,7 +53,8 @@
     line-height: 60px;
     height: 60px;
     font-size: 13px;
-    padding: 0 1.625rem;
+    font-family: "Times New Roman", Times, "serif";
+    padding: 0 1.875rem;
     &.login {
     background: url(../assets/images/lock.svg) no-repeat 0 50%;
     }
@@ -40,53 +89,6 @@ nav.navbar.menu {
 
 </style>
 
-<template>
-  <header>
-        <nav class="navbar py-0">
-            <div class="container-fluid px-0">
-                <a href="#" class="navbar-brand ms-4">
-                    <img src="../assets/images/logo.svg" alt="logo">
-                </a>
-                <ul class="nav justify-content-end">
-                    <li class="nav-item">
-                        <a class="nav-link login" href="#">LOGIN</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link signup" href="#">SIGN UP</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link wish" href="#">WISH LIST</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link cart" href="#">CART</a>
-                    </li>
-                    <li class="nav-item">
-                        <span class="nav-link search" href="#"></span>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-
-        <nav class="navbar menu">
-            <div class="container justify-content-center">
-                <ul class="nav justify-content-center">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">HOME</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">PRODUCTS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">SHOPPING GUIDE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">CONTACT</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-</template>
 
 <script>
 
