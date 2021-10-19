d6af1a1cbeeecefd96b022d2f6808def2e798eda
Nick <dreadnaughtlin@gmail.com>
2021-10-20 01:32:57 +0800

    modify style

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <template>
-  <div id="nav">
+  <div id="app">
   </div>
   <router-view />
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Noto+Serif+JP&family=Roboto&display=swap');
 @import "~@/assets/all.scss";
-
-
-#app {
-  color: #000;
-    font-family: Roboto, "Times New Roman", "Noto Serif JP", sans-serif;
-    background-color: #fff;
-    line-height: 1.8em;
-}
 
 </style>
