3a712d2d3e18096605a9808b2a854f38bdacbcef
Nick <dreadnaughtlin@gmail.com>
2021-11-18 17:11:41 +0800

    add Contact.vue & contact path with vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,43 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/products",
-    name: "Products",
-    component: () => import("../views/Products.vue"),
-  },
-  {
-    path: "/product",
-    name: "Product",
-    component: () => import("../views/Product.vue"),
-  },
+const routes = [{
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+    {
+        path: "/products",
+        name: "Products",
+        component: () =>
+            import ("../views/Products.vue"),
+    },
+    {
+        path: "/product",
+        name: "Product",
+        component: () =>
+            import ("../views/Product.vue"),
+    },
+    {
+        path: "/contact",
+        name: "Contact",
+        component: () =>
+            import ("../views/Contact.vue"),
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
---
+++ src/views/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <NavBar></NavBar>
+
+  <Footer></Footer>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Contact",
+  components: {
+    NavBar,
+    Footer,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
