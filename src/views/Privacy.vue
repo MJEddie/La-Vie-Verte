92aa94dbea2a7b5681a1d617f1a427a262be85bc
Nick <dreadnaughtlin@gmail.com>
2021-12-01 17:34:46 +0800

    add content to Privacy page

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -1,6 +1,132 @@
 <template>
   <NavBar></NavBar>
-  <main></main>
+  <main>
+    <div class="breadcrumb h-auto mb-0" style="--bs-breadcrumb-divider: '';">
+      <ol class="breadcrumb fw-light mb-0">
+        <li class="breadcrumb-item px-4">
+          <router-link to="/"><span>HOME</span></router-link>
+        </li>
+        <li class="breadcrumb-item px-4 active" aria-current="page">
+          <span>個人資料保護方針</span>
+        </li>
+      </ol>
+    </div>
+
+    <h1 class="title fs-4 text-center my-5 pb-3">個人資料保護方針</h1>
+
+    <section id="privacy">
+      <div class="container">
+        <div class="lead">
+          <h3 class="subtitle fw-bold fs-6">基本方針</h3>
+          <p>
+            La Vie Verte Co., Ltd.（ 以下稱「本公司」）依據個人資料保護法（
+            以下稱「個資法」）第八條第一項規定，向您告知下列事項，敬請詳閱。當您於本網站輸入個人資料完成登錄程序或開始使用本網站服務時，即視為您已清楚瞭解並同意以下本公司蒐集、處理或利用您個人資料之目的及用途。
+          </p>
+        </div>
+        <div class="lead">
+          <h3 class="subtitle fw-bold fs-6">
+            1. 關於個人資料的處理
+          </h3>
+          <ul class="list">
+            <li>
+              <span class="fw-bold">關於個人資訊之蒐集</span>
+              <p>
+                在蒐集個人資訊之際，對提供者明確說明蒐集目的，在達成此目的之必要限度內採取合法且公正之手段。
+              </p>
+            </li>
+            <li>
+              <span class="fw-bold">關於個人資訊之使用</span>
+              <p>在使用個人資訊之際，在獲得提供者同意之蒐集目的範圍內使用。</p>
+            </li>
+            <li>
+              <span class="fw-bold">關於個人資訊之提供及保管</span>
+              <p>
+                本公司將收集您的下列個人資料，以便提供服務。
+                姓名、性別、出生日期、地址、電話、電子郵件地址等辨識個人之資料；金融機構帳戶之號碼與姓名、信用卡或簽帳卡之號碼、保險單號碼、個人之其他號碼或帳戶等辨識財務之資料；個人描述及身體描述資料；消費習慣等，以便提供服務的個人資料。
+                但本公司可依蒐集目的之差異或需要，增刪所蒐集個人資料之分類與項目。
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div class="lead">
+          <h3 class="subtitle fw-bold fs-6">
+            2. 個人資料利用之期間、地區、對象及方式：
+          </h3>
+          <p>
+            1.
+            期間：特定目的之存續期間，或依法、依約應予保留之保存年限（如：商業會計法等），以最晚屆至者為準。
+          </p>
+          <p>
+            2.
+            對象：本公司、本公司國內外代理品牌企業、國內外集團關係企業、信用卡公司、金融聯合徵信中心、業務委外單位、未受中央目的事業主管機關限制之國際傳輸個人資料之接收者、依法有調查權機關或金融監理機關。
+          </p>
+          <p>
+            3. 地區：中華民國境內及前揭利用對象之所在地。
+          </p>
+          <p>
+            4. 方式：以自動化機器或其他非自動化之利用方式，包含國際傳輸。
+          </p>
+        </div>
+        <div class="lead">
+          <h3 class="subtitle fw-bold fs-6">
+            3. 個人資料之儲存及保管方式
+          </h3>
+          <p>
+            您的個人資料均被嚴密地保存在本網站的資料庫系統中。任何人均需在本公司訂定之權限管理規範下，始得進行資料之取得與利用，非經授權之人員，均不得以任何方法取得及利用您的個人資料。本網站以Secure
+            Sockets
+            Layer(SSL)進行個人資料傳輸的加密，並加裝防火牆防止不法入侵，避免您的個人資料在傳輸過程中遭非法擷取。另本網站也將您於資料庫中的密碼予以加密，以確保您的密碼不會遭到竊取。
+          </p>
+        </div>
+        <div class="lead">
+          <h3 class="subtitle fw-bold fs-6">
+            4.
+            非經您同意或未依法令規定者，本公司不會擅自修改任何您的個人資料。此外，依個資法第三條規定，就本公司所登錄或保存之您的個人資料，您得行使下列權利：
+          </h3>
+          <p>
+            1. 請求查詢、閲覧或製作複本。但本公司得酌收必要之成本費用。
+          </p>
+          <p>
+            2. 請求補充或更正個人資料。
+          </p>
+          <p>
+            3.
+            通知本公司停止蒐集、處理或利用個人資料。個人資料正確性有爭議時，原則上依您的請求停止處理或利用。但因執行業務所須並註明其爭議或經您書面同意者，不在此限。
+          </p>
+          <p>
+            4.
+            請求刪除個人資料。個人資料之蒐集目的消失或期限屆滿時，依您的請求刪除個人資料、停止處理或利用個人資料。但因執行業務所須或經您書面同意者不在此限。
+          </p>
+        </div>
+        <div class="lead">
+          <h3 class="subtitle fw-bold fs-6">
+            5. 不提供個人資料所致權益之影響
+          </h3>
+          <p>
+            您得自由選擇是否提供個人資料，不過，倘若您拒絕提供必要之個人資料，可能會影響本公司提供有關之服務。
+          </p>
+        </div>
+        <div class="lead">
+          <h3 class="subtitle fw-bold fs-6">
+            6. 免責聲明
+          </h3>
+          <p>
+            就透過網站所提供之資訊等，由 La Vie Verte Co., Ltd.（
+            以下稱「本公司」）
+            密切加以注意，惟不保證內容之真偽、可信性、正確性、瀏覽本網站不致生故障或錯誤或障礙、不存在電腦病毒等有害物。
+            此外，就自本網站連結之其它網站內容，本公司不負責任，或為任何保證。敬請見諒。
+          </p>
+        </div>
+        <div class="lead">
+          <h3 class="subtitle fw-bold fs-6">
+            7. 關於改版
+          </h3>
+          <p>
+            為進行營運、管理系統防護措施，將調整網站上之資訊（使用條款之變更除外）。另，可能中斷或中止網站之營運。敬請利用者隨時確認網站公告。
+          </p>
+        </div>
+      </div>
+    </section>
+  </main>
   <Footer></Footer>
 </template>
 
@@ -16,3 +142,77 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+.breadcrumb {
+  font-family: "Roboto", "Noto Sans Japanese", sans-serif;
+  background: #2f2f2f;
+  li {
+    background: #a9a9a9;
+    transform-origin: 0 100%;
+    transform: skewX(25deg);
+    font-size: 11px;
+    list-style: none;
+    border-left: 1px solid #2f2f2f;
+    &:last-child {
+      background: #2f2f2f;
+      color: #fff;
+      border-left: none;
+    }
+    a {
+      text-decoration: none;
+      color: #2f2f2f;
+    }
+    span {
+      display: block;
+      transform: skewX(-25deg);
+    }
+  }
+}
+
+h1.title {
+  font-family: Georgia, "Times New Roman", "Noto Serif JP", serif !important;
+  font-weight: 500;
+  letter-spacing: 0.1em;
+  position: relative;
+  &::after {
+    content: "";
+    width: 1.875rem;
+    border-bottom: 2px solid #000;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    margin-left: -15px;
+  }
+}
+
+#privacy {
+  margin-bottom: 108px;
+  @media screen and (max-width: 767px) {
+    padding: 0 20px;
+    margin-bottom: 30px;
+  }
+  @media screen and (min-width: 768px) and (max-width: 960px) {
+    padding: 0 30px;
+  }
+  .container {
+    max-width: 920px;
+    margin: 0 auto;
+    width: 100%;
+    .lead {
+      margin-bottom: 30px;
+      .subtitle {
+        margin-bottom: 20px;
+        padding: 0;
+      }
+      li span {
+        font-size: 14px;
+        letter-spacing: 0.1em;
+      }
+      p {
+        font-size: 13px;
+      }
+    }
+  }
+}
+</style>
